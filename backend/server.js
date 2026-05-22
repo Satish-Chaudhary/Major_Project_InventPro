3755f4bcde05a6bec9a99a473a3988c78177bbcb
Satish-Chaudhary <satish9619000@gmail.com>
2026-05-22 15:00:30 +0530

    setup deployment config

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(helmet({
 }));
 
 app.use(cors({
-    origin: "https://major-project-invent-pro.vercel.app/",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
 }))
